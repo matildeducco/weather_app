a7786d3beb7855a312090c968d5b75a667de1015
Matilde Ducco <matildedeschain@gmail.com>
2023-05-18 17:38:44 +0200

    Implemented forecast API and added autofocus to form

--- src/index.js
+++ src/index.js
@@ -13,6 +13,41 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+}
+
+function showForecast(response) {
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function(forecastDay, index) {
+        if (index < 6) {
+        forecastHTML = forecastHTML + `<div class="col-2 forecast">
+                <span class="forecast-day">${formatDay(forecastDay.dt)}</span>
+                <img
+                  src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+                  alt=""
+                  width="60"
+                  id="forescast-icon"
+                />
+                <span class="forecast-max">${Math.round(forecastDay.temp.max)}°</span> |
+                <span class="forecast-min">${Math.round(forecastDay.temp.min)}°</span>
+              </div>`;
+    }});
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+    let apiKey = "e450bc345a80a08ada69fd5c714d871d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showForecast);
+}
+
 function showTemperature(response) {
     let temperatureElement = document.querySelector("#temp");
     let cityElement = document.querySelector("#city");
@@ -60,33 +95,6 @@ let temperatureElement = document.querySelector("#temp");
 temperatureElement.innerHTML = Math.round(celsiusTemperature);
 celsiusLink.addClassList.add("active");
 fahrenheitLink.addClassList.remove("active");
-}
-
-function getForecast(coordinates) {
-    let apiKey = "e450bc345a80a08ada69fd5c714d871d";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayForecast);
-}
-
-function displayForecast(response) {
-    let forecastElement = document.querySelector("#forecast");
-    let forecastHTML = `<div class="row">`;
-    let days = ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
-    days.forEach(function(day) {
-        forecastHTML = forecastHTML + `<div class="col-2 forecast">
-                <span class="forecast-day">${day}</span>
-                <img
-                  src="https://openweathermap.org/img/wn/04d@2x.png"
-                  alt=""
-                  width="60"
-                  id="forescast-icon"
-                />
-                <span class="forecast-max">18°</span> |
-                <span class="forecast-min">12°</span>
-              </div>`
-    });
-    forecastHTML = forecastHTML + `</div>`;
-    forecastElement.innerHTML = forecastHTML;
 }
 
 let fahrenheitLink = document.querySelector("#fahrenheit");
